beb99717158f5d0f757bb851c23e94c367d7809e
Aditya <adityavit@gmail.com>
2010-11-10 22:52:04 +0530

    commiting the remaining files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,22 @@
 package controllers;
 
+import play.data.validation.Required;
 import play.mvc.*;
+import models.*;
 
 public class Application extends Controller {
 
+    @Before
+    static void bootStrapApp(){
+        User newUser = new User("demo","demo","demo");
+        newUser.persistNewUser();
+    }
     public static void index() {
         render();
+    }
+    
+    public static void deskScreen(User user){
+        render(user);
     }
 
 }
