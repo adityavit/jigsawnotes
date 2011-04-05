cc4df37db2c89de5753f41bf4e884d542ed3b0a4
Aditya <adityavit@gmail.com>
2011-04-05 11:33:31 +0530

    Adding Note Controller

---
+++ app/controllers/NoteController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.mvc.Controller;
+import models.*;
+import play.mvc.Before;
+
+public class NoteController extends Application {
+
+    @Before
+    static void checkUser() {
+        if (connected() == null) {
+            flash.error("Please log in first");
+            Application.index();
+        }
+    }
+
+    public static void addNewNote(Long id) {
+        User user = connected();
+        if (id == null) {
+
+        } else {
+            if (user != null) {
+                Desk selectedDesk = Desk.find("deskUser = ? and id = ?", user, id).first();
+                NoteJson newNoteObj = selectedDesk.addNewDeskNote();
+                renderJSON(newNoteObj);
+            }
+        }
+    }
+}
